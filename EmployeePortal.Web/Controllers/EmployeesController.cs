3d12a68bd025d2707110a706a20f6baea0f70148
MarkusWikman <Wikman.markus@gmail.com>
2024-04-14 12:04:40 +0200

    Done with Edit/put

--- EmployeePortal.Web/Controllers/EmployeesController.cs
+++ EmployeePortal.Web/Controllers/EmployeesController.cs
@@ -2,6 +2,7 @@
 using EmployeePortal.Web.Models;
 using EmployeePortal.Web.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeePortal.Web.Controllers
 {
@@ -35,6 +36,39 @@ namespace EmployeePortal.Web.Controllers
             await dbContext.SaveChangesAsync();
 
             return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var employees = await dbContext.Employees.ToListAsync();
+
+            return View(employees);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var employee = await dbContext.Employees.FindAsync(id);
+
+            return View(employee);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Employee viewModel)
+        {
+            var employee = await dbContext.Employees.FindAsync(viewModel.Id);
+
+            if(employee is not null)
+            {
+                employee.Name = viewModel.Name; 
+                employee.Phone = viewModel.Phone; 
+                employee.Position = viewModel.Position; 
+                employee.Salary = viewModel.Salary;
+                employee.Subscribed = viewModel.Subscribed;
+                employee.Email = viewModel.Email;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Employees");
         }
     }
 }
